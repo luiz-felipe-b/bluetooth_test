6d2a62d6fa71caa3cddff34bcb5f48682bd4b674
cadub <cadubas1@gmail.com>
2024-10-07 23:08:09 -0300

    feat: adds bluetooth serial connection to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:get/get.dart';
-import 'package:teste_bluetooth_pi/ble.controller.dart';
-void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+
+import './MainPage.dart';
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  List<BluetoothDevice> _devices = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _scanDevices();
-  }
-
-  Future<void> _scanDevices() async {
-    List<BluetoothDevice> devices = await BluetoothController().scanDevices();
-    setState(() {
-      _devices = devices;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bluetooth Devices'),
-      ),
-      body: ListView.builder(
-        itemCount: _devices.length,
-        itemBuilder: (context, index) {
-          BluetoothDevice device = _devices[index];
-          return ListTile(
-            title: Text(device.hashCode.toString()),
-            subtitle: Text(device.servicesList.toString()),
-          );
-        },
-      ),
-    );
+    return MaterialApp(home: MainPage());
   }
 }
